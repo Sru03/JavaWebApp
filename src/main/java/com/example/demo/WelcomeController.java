1bc09f2dbb6f0af9e4835b53fd19561736b58fe3
Srushti <mbsrushti@gmail.com>
2025-04-21 12:26:39 +1200

    changed application.properties to application.yaml but its not working

--- src/main/java/com/example/demo/WelcomeController.java
+++ src/main/java/com/example/demo/WelcomeController.java
@@ -3,12 +3,16 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-
 @RestController
 public class WelcomeController {
-    @GetMapping("/welcome")
+  
+   @GetMapping("/")
+   public String home(){
+    return "Welcome to home page";
+   }
 
+
+    @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to Spring Boot world";
     }
