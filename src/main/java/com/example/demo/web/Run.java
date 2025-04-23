5f19550c34155fc252f84749b40515ed6126c160
Srushti <mbsrushti@gmail.com>
2025-04-23 17:17:15 +1200

    demo app

---
+++ src/main/java/com/example/demo/web/Run.java
@@ -0,0 +1,29 @@
+package com.example.demo.web;
+import com.example.demo.web.Location;
+import java.time.LocalDateTime;
+
+import java.time.temporal.ChronoUnit;
+import org.springframework.stereotype.Repository;
+
+public record Run(    //we can't change or set values but can only get the values
+    Integer id,   
+    String title, 
+    LocalDateTime startedOn, 
+    LocalDateTime completedOn, 
+    Integer miles,
+    Location location
+   ) {
+
+        public Run(Integer id, String title, LocalDateTime startedOn, LocalDateTime completedOn, Integer miles, Location location ) {
+            this.id = id;
+            this.title = title;
+            this.startedOn = startedOn;
+            this.completedOn = completedOn;
+            this.miles = miles;
+            this.location = location;
+           
+        }
+
+}
+
+
