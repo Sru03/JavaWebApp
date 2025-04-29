9ec567d42899742808aecc998bf5069f23cdb201
Srushti <mbsrushti@gmail.com>
2025-04-29 18:49:47 +1200

    RunController

--- src/main/java/com/example/demo/web/RunController.java
+++ src/main/java/com/example/demo/web/RunController.java
@@ -1,5 +1,17 @@
 package com.example.demo.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RunController {
     
+
+    private final RunRepository runRepository;
+
+    public RunController(RunRepository runRepository){
+        this.runRepository = runRepository;
+    }
 }
