8b65e4507a7bc6db817a5de4d6fba088dfa9a23a
Srushti <mbsrushti@gmail.com>
2025-05-09 20:16:32 +1200

    added h2 database created run table inserted 1 run and can retrieve all runs

--- src/main/java/com/example/demo/web/RunRepository.java
+++ src/main/java/com/example/demo/web/RunRepository.java
@@ -7,59 +7,108 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Repository;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
 
 @Repository
 public class RunRepository {
 
-    private List<Run> runs = new ArrayList();
+       private static final Logger log = LoggerFactory.getLogger(RunRepository.class);
+       private final JdbcClient jdbcClient;
 
-    List<Run> findAll() {
-        return runs;
-    }
+       // jdbc api dependency injection 
+       public RunRepository (JdbcClient jdbcClient){
+        this.jdbcClient = jdbcClient; // it is going to auto configure a jdbc client 
+       }
 
-    Optional <Run> findById(Integer id){
-        return runs.stream()
-               .filter(run -> run.id() == id)
-               .findFirst();            
-    }
+       public List<Run> findAll(){
+        return jdbcClient.sql("select * from run")
+                .query(Run.class)
+                .list();
+       }
 
-    //to create run 
-     void create(Run run){
-        runs.add(run);
-     }
-
-     //updating the run by passing run and id 
-     void update(Run run, Integer id){
-        Optional<Run> existingRun = findById(id);
-        if(existingRun.isPresent()){
-            runs.set(runs.indexOf(existingRun.get()),run);
-        }
-     }
-
-     //delete the run by passing id
-     void delete(Integer id){
-        runs.removeIf(run -> run.id().equals(id));
-
-     }
+      
 
 
-    @PostConstruct
-    private void init() {
-        runs.add(new Run(1,
-                "Monday Morning Run",
-                LocalDateTime.now(),
-                LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
-                3,
-                Location.INDOOR));
 
-        runs.add(new Run(2,
-                "Wednesday Evening Run",
-                LocalDateTime.now(),
-                LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
-                6,
-                Location.INDOOR));
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // private List<Run> runs = new ArrayList();
+
+    // List<Run> findAll() {
+    //     return runs;
+    // }
+
+    // Optional <Run> findById(Integer id){
+    //     return runs.stream()
+    //            .filter(run -> run.id() == id)
+    //            .findFirst();            
+    // }
+
+    // //to create run 
+    //  void create(Run run){
+    //     runs.add(run);
+    //  }
+
+    //  //updating the run by passing run and id 
+    //  void update(Run run, Integer id){
+    //     Optional<Run> existingRun = findById(id);
+    //     if(existingRun.isPresent()){
+    //         runs.set(runs.indexOf(existingRun.get()),run);
+    //     }
+    //  }
+
+    //  //delete the run by passing id
+    //  void delete(Integer id){
+    //     runs.removeIf(run -> run.id().equals(id));
+
+    //  }
+
+
+    // @PostConstruct
+    // private void init() {
+    //     runs.add(new Run(1,
+    //             "Monday Morning Run",
+    //             LocalDateTime.now(),
+    //             LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
+    //             3,
+    //             Location.INDOOR));
+
+    //     runs.add(new Run(2,
+    //             "Wednesday Evening Run",
+    //             LocalDateTime.now(),
+    //             LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
+    //             6,
+    //             Location.INDOOR));
+
+    // }
 
   
 
--- src/test/java/com/example/demo/web/RunRepositoryTest.java
+++ src/test/java/com/example/demo/web/RunRepositoryTest.java
@@ -12,29 +12,29 @@ import java.util.List;
 public class RunRepositoryTest {
   
 
-    RunRepository repository;
-    @BeforeEach
-    void setUp(){
-        repository = new RunRepository();
-        repository.create(new Run (1,
-        "Monday Morning run",
-        LocalDateTime.now(), 
-        LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
-        3,
-        Location.INDOOR  ));
+    // RunRepository repository;
+    // @BeforeEach
+    // void setUp(){
+    //     repository = new RunRepository();
+    //     repository.create(new Run (1,
+    //     "Monday Morning run",
+    //     LocalDateTime.now(), 
+    //     LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
+    //     3,
+    //     Location.INDOOR  ));
 
-        repository.create(new Run (2,
-        "Wednesday Evening run",
-        LocalDateTime.now(), 
-        LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
-        6,
-        Location.INDOOR  ));
-    }
+    //     repository.create(new Run (2,
+    //     "Wednesday Evening run",
+    //     LocalDateTime.now(), 
+    //     LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
+    //     6,
+    //     Location.INDOOR  ));
+    // }
 
-    @Test
-    void shouldFindAllRuns(){
-        List<Run> runs = repository.findAll();
-        assertEquals(2, runs.size(), "Should returned 2 runs");
+    // @Test
+    // void shouldFindAllRuns(){
+    //     List<Run> runs = repository.findAll();
+    //     assertEquals(2, runs.size(), "Should returned 2 runs");
 
-    }
+    // }
 }
